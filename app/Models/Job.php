54adab2803c86f538f644739aab4dbc41ec486ed
oiseaubleuc <147993651+oiseaubleuc@users.noreply.github.com>
2024-08-11 01:49:25 +0200

    laravel

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,6 +9,16 @@ class Job extends Model{
     use HasFactory;
     protected $table= 'job_listings';
     protected $fillable = ['title', 'salary'];
+    public function employer ()
+    {
+        return $this->belongsTo(Employer::class);
+
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
+
+    }
 
 
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Employer extends Model
+class Tag extends Model
 {
     use HasFactory;
-    public function jobs()
-    {
-        return $this->hasMany(Job::class);
+    public function jobs(){
+        return $this->belongsToMany(Job::class,$relatedPivotKey: 'job_listing_id');
     }
 }
