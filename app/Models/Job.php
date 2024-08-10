e82f43decf2ee757178861f845a8f5398316ff06
oiseaubleuc <147993651+oiseaubleuc@users.noreply.github.com>
2024-08-10 18:52:21 +0200

    laravel

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,41 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
-{
-    public static function all(): array
-    {
+class Job extends Model{
+    protected $table= 'job_listings';
+    protected $fillable = ['title', 'salary'];
 
-        return [
-            [
-                'id' => 1,
-                'title' => 'director',
-                'salary' => '$59,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'teacher',
-                'salary' => '$2,000'
-            ]
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-
-        $job = Arr::first(static::all(), fn($job) => $job['id'] = $id);
-        if(!$job){
-            abort(404);
-        }
-        return $job;
-
-    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
 Route::get('/home', function () {
-    return view('home');
 });
 
 Route::get('/jobs', function () {
